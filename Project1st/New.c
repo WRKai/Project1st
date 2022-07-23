9d6acb90f5b5b03c1a69365bb1cf6878e8ae1fdf
QianFan <QianFan@LAPTOP-SKLHCP3B>
2022-07-23 20:13:28 +0800

    添加项目文件。

---
+++ Project1st/New.c
@@ -0,0 +1,7 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//extern int Global;
+//int main() {
+//	printf("%d\n", Global);
+//	return 0;
+//}
---
+++ Project1st/Test.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+//#define Ten 10
+//enum classtype {
+//	A=10,
+//	B,
+//	C,
+//	D,
+//	Z
+//};
+//int main() {
+//	enum classtype N = A;
+//	enum classtype N1 = B;
+//	enum classtype N2 = Z;
+//	printf("%d\n", N);
+//	printf("%d\n", N1);
+//	printf("%d\n", N2);
+//	return  0;
+//}
+//
+////int main() {
+////	double a = 0;	
+////	double b = 0;
+////	double conclusion = 0;
+////	scanf("%lf %lf", &a, &b);
+////	conclusion = a + b;
+////	if (conclusion > 10)
+////		printf("Result is %lf", conclusion);
+////	else
+////		printf("NT");
+////	return 0;
+////}
+int main(){
+	char b[] = {'a'};
+	char A[] = "abc";
+	printf("%s\n", A);
+	printf("\a\a\a");
+	return 0;
+}
